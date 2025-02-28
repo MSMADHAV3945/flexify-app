5a7ead19fb9baee921871c1391dcc9bb4d1db7c0
Madhav <m.s.madhav2007@gmail.com>
2025-03-01 01:18:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let swiper = new Swiper(".swiper-container" ,{
+    effect: "cube",
+    grabCursor: true,
+    loop: true,
+    navigation: {
+        nextEl : '.swiper-button-next',
+        prevEl : '.swiper-button-prev'
+    }
+})
